19e68da3ee172c9f09c5753cc8414bb4c51bd2fc
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-10-07 18:28:00 +0530

    Adding MyLinkedList class to add one by one nodes to LinkedList

--- src/main/java/MyDataStructures/MyNode.java
+++ src/main/java/MyDataStructures/MyNode.java
@@ -1,21 +1,32 @@
 package MyDataStructures;
 
-public class MyNode<K> {
+public class MyNode<K> implements INode<K> {
 
 	private K key;
-	private MyNode next;
+	private INode next;
 
 	public MyNode(K key) {
 		this.key = key;
 		this.next = null;
 	}
 
-	public void setNext(MyNode next) {
+	public void setNext(INode next) {
 		this.next = next;
 	}
 
-	public MyNode getNext() {
+	public INode getNext() {
 		return next;
+	}
+
+	@Override
+	public K getKey() {
+		return key;
+	}
+
+	@Override
+	public void setKey(K Key) {
+		this.key = key;
+		
 	}
 
 }
